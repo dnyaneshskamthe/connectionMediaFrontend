4ee00d36158c00e3ff06ff3a6eb337a56eab13ba
Dnyanesh SK <dnyanesh451989@gmail.com>
2022-06-15 11:49:45 +0530

    added logout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 
 
 
 const HomePage = () => {
+  const nav = useNavigate();
+  const userLogout=()=>{
+    axios.post("http://localhost:5000/api/v1/user/register",
+    {       
+            mode:'cors',
+            headers:{
+              'Access-Control-Allow-Origin':'*',
+              "Access-Control-Allow-Credentials" : true }
+            
+    }).then((response)=>{
+        console.log(response);
+        alert('User Logged Out Successfully!');
+        nav('/landing');
+
+    })
   return (
     <>
     <h1>
-this is home page
+        this is home page
     </h1>
+    <h3 onClick={userLogout}>Logout</h3>
     </>
   )
-}
+} }
 
-export default HomePage
+export default HomePage;
